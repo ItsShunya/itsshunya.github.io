fe0835e209b9f3fe5747d31873c66df9b231aff9
Victor Luque <luque.viictor@gmail.com>
2025-09-12 19:55:53 +0200

    dev: nix develop flake

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Dev environment flake for the site's development";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          # Add runtime dependencies, language, and libraries here
+          buildInputs = with pkgs; [
+            git
+            hugo
+          ];
+        };
+      }
+    );
+}
